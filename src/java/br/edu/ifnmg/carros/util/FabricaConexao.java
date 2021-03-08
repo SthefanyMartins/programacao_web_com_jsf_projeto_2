9e9182d335f6ae01695cf5c34a2385646b4d6d73
Sthefany Martins <sthefanymartins1505@gmail.com>
2021-03-08 10:01:00 -0300

    aula 5.2

--- src/java/br/edu/ifnmg/carros/util/FabricaConexao.java
+++ src/java/br/edu/ifnmg/carros/util/FabricaConexao.java
@@ -9,25 +9,20 @@ import java.util.logging.Logger;
 public class FabricaConexao {
     
     private static Connection conexao;
-    private static final String URL_CONEXAO = "jdbc:mysql://localost/sistema_carros";
-    private static final String USUARIO = "root";
-    private static final String SENHA = "123456";
+    private static final String URL_CONEXAO = "jdbc:mysql://localhost:3306/sistema_carros";
+    private static final String USUARIO = "sistema_carros";
+    private static final String SENHA = "654321";
 
     public static Connection getConexao() {
         
         if(conexao == null){
-           
             try {
-                Class.forName("com.mysql.jdbc.Driver");
-                conexao = DriverManager.getConnection(URL_CONEXAO, USUARIO, SENHA);
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                conexao = DriverManager.getConnection(URL_CONEXAO + "?useTimezone=true&serverTimezone=America/Sao_Paulo&user="+USUARIO+"&password="+SENHA);
             } catch (SQLException ex) {
                 Logger.getLogger(FabricaConexao.class.getName()).log(Level.SEVERE, null, ex);
-                System.out.println("\nNao foi possivel conectar ao Banco de Dados.\n");
-                return null;
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(FabricaConexao.class.getName()).log(Level.SEVERE, null, ex);
-                System.out.println("O driver expecificado nao foi encontrado.");
-                return null;
             }   
         }
         return conexao;
